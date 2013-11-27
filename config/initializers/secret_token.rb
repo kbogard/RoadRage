afe88039cb0c7cd10ed7eda493a02b3810cdbf59
Kathy <kg@sande.com>
2013-11-27 16:43:12 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoadRage::Application.config.secret_token = '757c010d2812a4f82086bbfbcac8b8d6a90ad29b3fb51e5a538e6b6b428e39d043b2b4683de624e447c454467e11a321172cea5d5a993584cffe1ecc5526594f'
